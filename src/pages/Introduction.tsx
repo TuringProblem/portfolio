d4fb79affc9d0f02d1f5378d14df296e6efa198d
Override <122590935+TuringProblem@users.noreply.github.com>
2025-11-11 23:21:52 -0500

    feat: making some good progress on top bar

--- src/pages/Introduction.tsx
+++ src/pages/Introduction.tsx
@@ -4,6 +4,8 @@ import { Download, Linkedin, Github, Twitter} from 'lucide-react';
 
 
 const GITHUB_PROFILE = "https://github.com/TuringProblem";
+const TWITTER_PROFILE = "https://twitter.com/TuringProblem";
+const LINKEDIN_PROFILE = "https://www.linkedin.com/in/andrew-wellington-37a665246/";
 
 const redirect = (url: string) => {
   window.location.href = url;
@@ -27,7 +29,7 @@ export const Introduction: React.FC = () => {
           </div>
         </div>
 
-        <div className="flex-col bg-[var(--bg-secondary)]] border border-[var(--border)] border-radius-[var(--terminal-radius)] rounded-md shadow-lg">
+        <div className="flex-col bg-[var(--bg-secondary)]]] ">
 
           <div className="py-10 flex justify-between">
             <h1 className="text-4xl max-w-[300px] font-bold text-[var(--text-primary)] flex justify-between items-center">
@@ -37,10 +39,10 @@ export const Introduction: React.FC = () => {
               <button className="flex gap-1" onClick={() => redirect(GITHUB_PROFILE)}>
                 <Github size={24} /> Github Profile
               </button>
-              <button className="flex gap-1">
+              <button className="flex gap-1" onClick={() => redirect(TWITTER_PROFILE)}>
                 <Twitter size={24} /> (𝕏)* Account
               </button>
-              <button className="flex gap-1">
+              <button className="flex gap-1" onClick={() => redirect(LINKEDIN_PROFILE)}>
                 <Linkedin size={24} /> Linkedin
               </button>
               <button className="flex  gap-1">
@@ -48,7 +50,12 @@ export const Introduction: React.FC = () => {
               </button>
             </div>
             
-            <img src="https://media.giphy.com/media/l0MYd5o7l9g8o/giphy.gif" alt="gif" className="w-full rounded-full w-[100px] h-[100px]" />
+            <div className="mt-6 max-w-[700px] min-w-[400px] text-sm text-[var(--accent)] bg-[var(--bg-secondary)] border border-[var(--accent)] shadow-lg rounded-lg">
+            <div className="bg-[var(--border)] mt-3.5 mx-4 align-center rounded-md">
+              I'm Andrew, a Computer Science student at Northeastern University. Academically, I'm interested in Compilers, Programming Languages, Mathematics, and Philosophy. With respect to leasurely activities - I enjoy programming, <a href="https://">music</a>, and sports.   
+            </div>
+            </div>
+            <img src="https://media.giphy.com/media/l0MYd5o7l9g8o/giphy.gif" alt="gif" className="w-full rounded-full w-[150px] h-[150px]" />
           </div>
           <div className="text-[var(--text-secondary)]">
               <div className="max-w-[75vw]">
