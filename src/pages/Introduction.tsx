9d7b1d43ce2874684c6d9e503452c75796eda78f
Override <122590935+TuringProblem@users.noreply.github.com>
2025-11-11 22:23:37 -0500

    feat: starting to cook up on the main page

--- src/pages/Introduction.tsx
+++ src/pages/Introduction.tsx
@@ -1,5 +1,13 @@
 import { BreadcrumbButton } from '../components/buttons/';
 import { ThemeToggle } from '../components/ThemeToggle';
+import { Download, Linkedin, Github, Twitter} from 'lucide-react';
+
+
+const GITHUB_PROFILE = "https://github.com/TuringProblem";
+
+const redirect = (url: string) => {
+  window.location.href = url;
+}
 
 export const Introduction: React.FC = () => {
   return (
@@ -18,15 +26,38 @@ export const Introduction: React.FC = () => {
             Introduction/
           </div>
         </div>
-        <div className="py-10 flex flex-col">
-          <h1 className="text-4xl font-bold text-[var(--text-primary)] flex justify-between items-center">
-            Welcome! 👋
-          <img src="https://media.giphy.com/media/l0MYd5o7l9g8o/giphy.gif" alt="gif" className="w-full rounded-full w-[100px] h-[100px]" />
-          </h1>
-          <div className="text-[var(--text-secondary)]">
-            Welcome to my portfolio introduction
+
+        <div className="flex-col bg-[var(--bg-secondary)]] border border-[var(--border)] border-radius-[var(--terminal-radius)] rounded-md shadow-lg">
+
+          <div className="py-10 flex justify-between">
+            <h1 className="text-4xl max-w-[300px] font-bold text-[var(--text-primary)] flex justify-between items-center">
+              Welcome! 👋
+            </h1>
+            <div className="flex flex-col gap-2">
+              <button className="flex gap-1" onClick={() => redirect(GITHUB_PROFILE)}>
+                <Github size={24} /> Github Profile
+              </button>
+              <button className="flex gap-1">
+                <Twitter size={24} /> (𝕏)* Account
+              </button>
+              <button className="flex gap-1">
+                <Linkedin size={24} /> Linkedin
+              </button>
+              <button className="flex  gap-1">
+                <Download size={24} /> Resume
+              </button>
+            </div>
+            
+            <img src="https://media.giphy.com/media/l0MYd5o7l9g8o/giphy.gif" alt="gif" className="w-full rounded-full w-[100px] h-[100px]" />
           </div>
+          <div className="text-[var(--text-secondary)]">
+              <div className="max-w-[75vw]">
+                This is my portfolio bitch, welcome to this place. 
+              </div>
+          </div>
+
         </div>
+
       </div>
     </div>
   );
