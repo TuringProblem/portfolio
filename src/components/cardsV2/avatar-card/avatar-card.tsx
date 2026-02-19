c00b11b0ad67caa9aeea91b059606c585b04ea52
Override <122590935+TuringProblem@users.noreply.github.com>
2026-02-19 11:28:35 -0500

    fix: oh it's looking elegant

--- src/components/cardsV2/avatar-card/avatar-card.tsx
+++ src/components/cardsV2/avatar-card/avatar-card.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react';
 import { BaseCard } from '../base';
 import './card-style.css';
 
-export type Background = 'massasoit' | 'northeastern';
+type Background = 'massasoit' | 'northeastern';
 
 export interface AvatarCardProps {
   img?: string;
@@ -16,16 +16,14 @@ interface AvatarData {
 }
 
 const backgroundMap: Record<Background, string[]> = {
-  massasoit: ['bg-[var(--avatar-card-massasoit)]', 'border-[var(--avatar-card-border-massasoit)]', 'flex-grid grid-cols-2'],
+  massasoit: ['bg-[var(--avatar-card-massasoit)]', 'border-[var(--avatar-card-border-massasoit)]', 'flex-grid grid-cols-2 sm:grid-cols-1'],
   northeastern: ['bg-[var(--avatar-card-northeastern)]', 'border-[var(--avatar-card-border-northeastern)]', 'flex-grid grid-cols-3'],
-
 }
 
 const handleBg = (bg: Background): string[] => {
   const values = backgroundMap[bg];
   return [...values];
 }
-
 
 export const AvatarCard: FC<AvatarData> = ({ data }) => {
   const [bgClass, borderClass, gridSize] = handleBg(data[0].bg);
@@ -40,7 +38,7 @@ export const AvatarCard: FC<AvatarData> = ({ data }) => {
             <li key={index} className="text-[12px] justify-start">{lesson}</li>
           ))}
         </BaseCard>
-      ))};
+      ))}
     </div>
   );
 }
