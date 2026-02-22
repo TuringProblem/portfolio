5468b167a8323e1adb040397b653d3eed267338a
Override <122590935+TuringProblem@users.noreply.github.com>
2026-02-21 22:17:28 -0500

    chore: push for arch

--- src/components/cardsV2/text-field-card/text-field-card.tsx
+++ src/components/cardsV2/text-field-card/text-field-card.tsx
@@ -6,16 +6,20 @@ import { grabTextAndPaddingSizes } from '../../../utils';
 
 
 
+interface TextFieldCardProps {
+  position: string;
+  size?: Sizes;
+}
+
 /**
  * @author { @Override }
  * @since 02012026
  * @see <a href="https://github.com/TuringProblem">Github Profile</a>
  **/
-export const TextFieldCard: FC<{ position: string, size?: Sizes }> = ({ position, size }) => {
+export const TextFieldCard: FC<TextFieldCardProps> = ({ position, size }) => {
   return (
     <div>
       <Header title="Text Field" size={size || "md"} position={position} />
-
       <BaseCard className="flex flex-col gap-[16px] bg-[var(--about-card-background)] border border-transparent hover:border-[var(--ifm-color-primary)] shadow-[0px_0.5px_1px_0px] hover:shadow-[0px_0.5px_1px_0px] hover:cursor-pointer">
         <input type="text" placeholder="Enter your text here" className="w-full h-full bg-transparent border-none outline-none" />
       </BaseCard>
