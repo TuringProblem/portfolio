da515aec26df60eb21db7a4542e7010971f412ec
Override <122590935+TuringProblem@users.noreply.github.com>
2026-05-02 20:37:14 -0400

    chore: style

--- src/Components/Carousel/Carousel.elm
+++ src/Components/Carousel/Carousel.elm
@@ -5,8 +5,7 @@ import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
 import Html.Keyed as Keyed
 
--- author: { @Override } : Since: 20260725 @1604
-
+-- author: { @Override } : Since: 20260802 @0950
 
 viewCarousel : msg -> msg -> Int -> List String -> Html msg
 viewCarousel onPrev onNext currentIndex imageUrls =
@@ -15,50 +14,24 @@ viewCarousel onPrev onNext currentIndex imageUrls =
             div [] []
 
         _ ->
-            let
-                total =
-                    List.length imageUrls
-
-                safeIndex =
-                    modBy total currentIndex
-
-                current =
-                    imageUrls |> List.drop safeIndex |> List.head
-            in
+            let total = List.length imageUrls
+                safeIndex = modBy total currentIndex
+                current = imageUrls |> List.drop safeIndex |> List.head in
             div [ class "carousel" ]
                 [ case current of
                     Nothing ->
                         div [] []
-
                     Just url ->
                         Keyed.node "div"
                             [ class "carousel-track" ]
                             [ ( String.fromInt safeIndex
-                              , img
-                                    [ class "carousel-image"
-                                    , src url
-                                    ]
-                                    []
+                              , img [ class "carousel-image", src url] []
                               )
                             ]
                 , div [ class "carousel-controls" ]
                     [ button [ class "carousel-btn", onClick onPrev ] [ text "←" ]
                     , div [ class "carousel-dots" ]
-                        (List.indexedMap
-                            (\i _ ->
-                                span
-                                    [ class
-                                        (if i == safeIndex then
-                                            "carousel-dot carousel-dot--active"
-
-                                         else
-                                            "carousel-dot"
-                                        )
-                                    ]
-                                    []
-                            )
-                            imageUrls
-                        )
+                        (List.indexedMap (\i _ -> span [ class (if i == safeIndex then "carousel-dot carousel-dot--active" else "carousel-dot") ] []) imageUrls)
                     , button [ class "carousel-btn", onClick onNext ] [ text "→" ]
                     ]
                 ]
