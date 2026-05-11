f23ab257e655f7ff522145814e260ff57c6ee4f5
Override <122590935+TuringProblem@users.noreply.github.com>
2026-05-11 11:55:57 -0400

    small changes

--- src/Projects/Deej.elm
+++ src/Projects/Deej.elm
@@ -12,5 +12,5 @@ project =
     , url         = "https://github.com/TuringProblem/deej"
     , tags        = [ "C" ]
     , additionalText = Nothing
-    , imageUrls    = Nothing
+    , imageUrls    = Just ["https://private-user-images.githubusercontent.com/122590935/368389727-4c6d22d8-7583-4079-9992-f0ebe3ebbd4e.gif?jwt=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3Nzg1MTQzMzMsIm5iZiI6MTc3ODUxNDAzMywicGF0aCI6Ii8xMjI1OTA5MzUvMzY4Mzg5NzI3LTRjNmQyMmQ4LTc1ODMtNDA3OS05OTkyLWYwZWJlM2ViYmQ0ZS5naWY_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjYwNTExJTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI2MDUxMVQxNTQwMzNaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT00N2MwOGM2MWNkMDI2MDg3ZjU2NzdlMWQzMTQ5YjM1YWRjNzNiNDEyYTljYWEyMzYwZjkxYWUxNTgzMTlmNjRmJlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCZyZXNwb25zZS1jb250ZW50LXR5cGU9aW1hZ2UlMkZnaWYifQ.yemgfW1Tlc085dWmUiTY7tRG3Xu-PZlt18b6_wP2W8E"] 
     }
